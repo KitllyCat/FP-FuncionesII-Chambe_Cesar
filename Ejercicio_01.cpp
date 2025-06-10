f4510ec5fc1879f5277a6d29c4e5ce465d7ff1cf
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-10 13:49:33 -0500

    Ejercicio 01, calcular area de un circulo

---
+++ Ejercicio_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main(){
+	SetConsoleOutputCP(CP_UTF8);
+	double radio;
+	cout<<"---Calcular área de un circulo---"<<endl<<endl;
+	cout<<"Ingrese el radio del circulo: ";
+	cin>>radio;
+	cout<<"El area del circulo es de "<<(pow(radio, 2)*(M_PI))<<"."<<endl;
+		
+	return 0;
+}
