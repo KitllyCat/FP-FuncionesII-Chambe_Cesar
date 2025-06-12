b41ea45987c3b9b2a4f4ab07aca5929bd0f83704
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-11 22:24:31 -0500

    Se añadio funciones para comprobar si es un raiz cuadratica, y calcular raices reales y complejas, versión final(se elimino las 4 primeras lineas)

--- Ejercicio_04.cpp
+++ Ejercicio_04.cpp
@@ -1,7 +1,3 @@
-/*Cálculo de Raíces de una Ecuación Cuadrática: Crea un programa que calcule las raíces de una ecuación cuadrática
-utilizando la fórmula general. Utiliza la función sqrt de la librería cmath para calcular la raíz cuadrada. Solicita
-al usuario los coeficientes de la ecuación (a, b, c) y muestra las raíces obtenidas. Considera los casos en los que
-las raíces son reales o complejas.*/
 #include <iostream>
 #include <cmath>
 using namespace std;
