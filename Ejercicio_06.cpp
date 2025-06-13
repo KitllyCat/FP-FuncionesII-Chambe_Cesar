5f44a651cd972a040c0e7a844ecd4f030ad3e63a
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-12 23:02:53 -0500

    Version final del juego, con la gui terminada y las funciones correspondientes

--- Ejercicio_06.cpp
+++ Ejercicio_06.cpp
@@ -1,88 +1,203 @@
-/*Juego de Piedra, Papel y Tijeras: Desarrolla un programa que simule el juego de piedra, papel y tijeras contra
-la computadora. El usuario elegirá su jugada (piedra, papel o tijeras) y la computadora generará su jugada de forma
-aleatoria. El programa determinará el ganador de cada ronda según las reglas del juego. El juego continuará hasta
-que el usuario o la computadora gane 3 rondas. Muestra el resultado final indicando el ganador del juego.*/
 #include <iostream>
 #include <windows.h>
 #include <ctime>
 #include <cstdlib>
 using namespace std;
 
+void tijera(){
+	cout << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░██░░░░░░░░" << endl;
+	cout << "░░░░░░░░█░░█░░░░░░░" << endl;
+	cout << "░░░░██░░█░░█░░░░░░░" << endl;
+	cout << "░░░█░░█░█░░█░░░░░░░" << endl;
+	cout << "░░░█░░█░█░░█░░░░░░░" << endl;
+	cout << "░░░█░░█░█░░█░░░░░░░" << endl;
+	cout << "░░░█░░░██░░█░░░░░░░" << endl;
+	cout << "░░░░█░░██░░█░░░░░░░" << endl;
+	cout << "░░░░█░░██░░███░░░░░" << endl;
+	cout << "░░░░█░░░█░░█░░██░░░" << endl;
+	cout << "░░░░░█░░█░░█░░█░█░░" << endl;
+	cout << "░███░█░░█░░█░░█░░█░" << endl;
+	cout << "░█░░██░░░░░░░░█░░█░" << endl;
+	cout << "░█░░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░█░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░░░░░░░░░░░░░█░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░█░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░██████████░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;	
+}
+void piedra(){
+	cout << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░██░░░░░░░░" << endl;
+	cout << "░░░░░░███░░█░░░░░░░" << endl;
+	cout << "░░░░░█░░█░░████░░░░" << endl;
+	cout << "░░░░░█░░█░░█░░██░░░" << endl;
+	cout << "░░░░░█░░█░░█░░█░█░░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░███░█░░░░░░░░█░░█░" << endl;
+	cout << "░█░░██░░░░░░░░░░░█░" << endl;
+	cout << "░█░░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░█░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░░░░░░░░░░░░░█░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░█░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░██████████░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+}
+void papel(){
+	cout << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+	cout << "░░░░░░░░░██░░░░░░░░" << endl;
+	cout << "░░░░░░███░░█░░░░░░░" << endl;
+	cout << "░░░░░█░░█░░████░░░░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░░░" << endl;
+	cout << "░░░░░█░░█░░█░░███░░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░░░░░█░░█░░█░░█░░█░" << endl;
+	cout << "░███░█░░█░░█░░█░░█░" << endl;
+	cout << "░█░░██░░░░░░░░█░░█░" << endl;
+	cout << "░█░░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░█░░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░█░░░░░░░░░░░█░" << endl;
+	cout << "░░░█░░░░░░░░░░░░░█░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░█░░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░░█░░" << endl;
+	cout << "░░░░░█░░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░█░░░░░░░░█░░░" << endl;
+	cout << "░░░░░░██████████░░░" << endl;
+	cout << "░░░░░░░░░░░░░░░░░░░" << endl;
+}
+
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	srand(time(0));
 	int com_eleccion, eleccion;
 	int wins=0, com_wins=0;
-	//se quito porque al final resulto siendo basura
 
 	do{
-		cout<<endl<<"---Piedra, papel o tijera---"<<endl;
-		cout<<"Ingrese su eleccion(1,2,3):"<<endl;
-		cout<<"1)Piedra"<<endl;
-		cout<<"2)Papel"<<endl;
-		cout<<"3)Tijera"<<endl;
-		cin>>eleccion;
+		do{
+			cout<<"---Piedra, papel o tijera---"<<endl;
+			cout<<"Ingrese su eleccion(1,2,3):"<<endl;
+			cout<<"1)Piedra"<<endl;
+			cout<<"2)Papel"<<endl;
+			cout<<"3)Tijera"<<endl;
+			cin>>eleccion;
 		
+			if(eleccion>3 || eleccion<1){
+				cout<<"Opcion Invalida"<<endl;
+			}else{
+				break;
+			}
+		}while(true);
+		
+		system("cls");
 		cout<<"Tu jugada es de '";
 	
 		if(eleccion==1){
 			cout<<"piedra'"<<endl;
+			piedra();
 		}else if(eleccion==2){
 			cout<<"papel'"<<endl;
+			papel();
 		}else{
 			cout<<"tijera'"<<endl;
+			tijera();
 		}
-	
-		cout<<"La jugada de la maquina es de...  '";	
+		Sleep(2000);
+		
+		system("cls");
+		cout<<"La jugada de la maquina es de...  "<<endl;
 		com_eleccion = rand()%3+1;
-	
+		cout<<"3..."<<endl;
+		Sleep(1000);
+		cout<<"2..."<<endl;
+		Sleep(1000);
+		cout<<"1..."<<endl;
+		Sleep(1000);
+		
 		if(com_eleccion==1){
-			cout<<"piedra'"<<endl;
+			cout<<"PIEDRA!!!"<<endl;
+			piedra();
 		}else if(com_eleccion==2){
-			cout<<"papel'"<<endl;
+			cout<<"PAPEL!!!"<<endl;
+			papel();
 		}else{
-			cout<<"tijera'"<<endl;
+			cout<<"TIJERA!!!"<<endl;
+			tijera();
 		}
+		Sleep(3000);
 		
 		switch(eleccion){
         	case 1: // Piedra
         	    if (com_eleccion == 1){
         	        cout<<"Empate"<<endl;
+        	        Sleep(2000);
         	    }else if (com_eleccion == 2){
         	        cout<<"Perdiste"<<endl;
         	        com_wins++;
+        	        Sleep(2000);
 				}else{
         	        cout<<"Ganaste"<<endl;
         	        wins++;
+        	        Sleep(2000);
         	    }
         	    break;
         	case 2: // Papel
         	    if (com_eleccion == 1){
         	        cout<<"Ganaste"<<endl;
         	        wins++;
+        	        Sleep(2000);
         	    }else if(com_eleccion == 2){
         	        cout<<"Empate"<<endl;
+        	        Sleep(2000);
         	    }else{
         	        cout<<"Perdiste"<<endl;
         	        com_wins++;
+        	        Sleep(2000);
         		}
         	    break;
         	case 3: // Tijera
         	    if (com_eleccion == 1){
         	        cout<<"Perdiste"<<endl;
         	        com_wins++;
+        	        Sleep(2000);
 				}else if (com_eleccion == 2){
         	        cout<<"Ganaste"<<endl;
         	        wins++;
+        	        Sleep(2000);
         	    }else{
         	        cout<<"Empate"<<endl;
+        	        Sleep(2000);
         	    }
         	    break;
         	default:
-        	    cout<<"Opción inválida"<<endl;
+        	    cout<<"Error"<<endl;
         	    break;
     	}
     	cout<<endl<<"Actualmente vas "<<wins<<" victorias y la computadora va "<<com_wins<<" victorias."<<endl;
+    	Sleep(3000);
+    	system("cls");
     	
 	}while(wins<3 && com_wins<3);
 
