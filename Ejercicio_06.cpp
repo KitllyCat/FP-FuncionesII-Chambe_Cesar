1ac100d173753ebc52ce67ff00cbf08396eafb23
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-12 21:33:21 -0500

    Se termino lo restante respecto a la logica, no obstante, aun falta implementar lo visual para el juego

--- Ejercicio_06.cpp
+++ Ejercicio_06.cpp
@@ -12,49 +12,85 @@ int main(){
 	SetConsoleOutputCP(CP_UTF8);
 	srand(time(0));
 	int com_eleccion, eleccion;
-	char com_jugada, jugada;
-	char p,r,t; //p = papel, r = piedra, t = tijera
+	int wins=0, com_wins=0;
+	//se quito porque al final resulto siendo basura
 
 	do{
 		cout<<endl<<"---Piedra, papel o tijera---"<<endl;
-		cout<<"Ingrese su eleccion():"<<endl;
+		cout<<"Ingrese su eleccion(1,2,3):"<<endl;
 		cout<<"1)Piedra"<<endl;
 		cout<<"2)Papel"<<endl;
 		cout<<"3)Tijera"<<endl;
 		cin>>eleccion;
 		
-		if (eleccion>4||eleccion<0){
-			cout<<"Eleccion no valida!!!"<<endl;
+		cout<<"Tu jugada es de '";
+	
+		if(eleccion==1){
+			cout<<"piedra'"<<endl;
+		}else if(eleccion==2){
+			cout<<"papel'"<<endl;
+		}else{
+			cout<<"tijera'"<<endl;
+		}
+	
+		cout<<"La jugada de la maquina es de...  '";	
+		com_eleccion = rand()%3+1;
+	
+		if(com_eleccion==1){
+			cout<<"piedra'"<<endl;
+		}else if(com_eleccion==2){
+			cout<<"papel'"<<endl;
+		}else{
+			cout<<"tijera'"<<endl;
 		}
 		
-	}while(eleccion>4||eleccion<0);
-	
-	cout<<"Tu jugada es de '";
-	
-	if(eleccion==1){
-		jugada=r;
-		cout<<"piedra'"<<endl;
-	}else if(eleccion==2){
-		jugada=p;
-		cout<<"papel'"<<endl;
+		switch(eleccion){
+        	case 1: // Piedra
+        	    if (com_eleccion == 1){
+        	        cout<<"Empate"<<endl;
+        	    }else if (com_eleccion == 2){
+        	        cout<<"Perdiste"<<endl;
+        	        com_wins++;
+				}else{
+        	        cout<<"Ganaste"<<endl;
+        	        wins++;
+        	    }
+        	    break;
+        	case 2: // Papel
+        	    if (com_eleccion == 1){
+        	        cout<<"Ganaste"<<endl;
+        	        wins++;
+        	    }else if(com_eleccion == 2){
+        	        cout<<"Empate"<<endl;
+        	    }else{
+        	        cout<<"Perdiste"<<endl;
+        	        com_wins++;
+        		}
+        	    break;
+        	case 3: // Tijera
+        	    if (com_eleccion == 1){
+        	        cout<<"Perdiste"<<endl;
+        	        com_wins++;
+				}else if (com_eleccion == 2){
+        	        cout<<"Ganaste"<<endl;
+        	        wins++;
+        	    }else{
+        	        cout<<"Empate"<<endl;
+        	    }
+        	    break;
+        	default:
+        	    cout<<"Opción inválida"<<endl;
+        	    break;
+    	}
+    	cout<<endl<<"Actualmente vas "<<wins<<" victorias y la computadora va "<<com_wins<<" victorias."<<endl;
+    	
+	}while(wins<3 && com_wins<3);
+
+	if(wins == 3){
+		cout<<"¡Ganaste el juego!"<<endl;
 	}else{
-		jugada=t;
-		cout<<"tijera'"<<endl;
+		cout<<"La computadora ganó el juego. ¡Mejor suerte la próxima vez!"<<endl;
 	}
 	
-	cout<<"La jugada de la maquina es de...  '";	
-	com_eleccion = rand()%3+1;
-	
-	if(com_eleccion==1){
-		com_jugada=r;
-		cout<<"piedra'"<<endl;
-	}else if(com_eleccion==2){
-		com_jugada=p;
-		cout<<"papel'"<<endl;
-	}else{
-		com_jugada=t;
-		cout<<"tijera'"<<endl;
-	}	
-	        
 	return 0;
 }
