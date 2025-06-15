e2d80b629a5ec126b9d0fc412b86fc0506187fd0
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-14 22:47:06 -0500

    Version final del juego, se añadio elementos visules faltantes y se mejoro el codigo en general

--- Ejercicio_07.cpp
+++ Ejercicio_07.cpp
@@ -7,83 +7,236 @@ ganador del juego. Muestra el resultado de cada ronda y el marcador acumulado. U
 #include <cstdlib>
 using namespace std;
 
+void dado1() {
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░█░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
+}
 
-void dado1(){
-	
+void dado2() {
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
 }
-void dado2(){
-	
-}
+
 void dado3(){
-	
-}
-void dado4(){
-	
-}
-void dado5(){
-	
-}
-void dado6(){
-	
-}
-
-void dadoAnimacion(){
-	
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░█░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
 }
 
-int main(){
-	SetConsoleOutputCP(CP_UTF8);
-	srand(time(0));
-	int jugada1,jugada2;
-	int wins1=0, wins2=0, rondas=1;
-	
-	cout << "--- Juego de dados ---" << endl << endl;
-	cout<<"Por cada turno el jugador 1 y el jugador tiraran el dado..."<<endl;
-	cout<<"Presiona enter para comenzar el juego: ";
-	cin.get();
-	system("cls");	
+void dado4() {
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
+}
 
-	do{
-		cout<<rondas<<"._Ronda: ";
-		cin.get();
-		system("cls");
+void dado5() {
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░░░█░░░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
+}
+
+void dado6() {
+    cout << "░░░░░░░░░░░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█░█░░░█░█░" << endl;
+    cout << "░█░░░░░░░█░" << endl;
+    cout << "░█████████░" << endl;
+    cout << "░░░░░░░░░░░" << endl;
+    cout << endl;
+}
+
+void dadoAnimacion() {
+    cout<<"Tu dado que salio es de..."<<endl;
+	dado6(); Sleep(100); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado3(); Sleep(100); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado5(); Sleep(150); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado2(); Sleep(150); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado4(); Sleep(200); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado1(); Sleep(200); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado3(); Sleep(300); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado6(); Sleep(300); system("cls");
+	cout<<"Tu dado que salio es de..."<<endl;
+	dado2(); Sleep(400); system("cls");
+	cout<<endl;
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    srand(time(0));
+    int jugada1,jugada2;
+    int wins1=0, wins2=0, rondas=1;
+
+    cout << "--- Juego de dados ---" << endl << endl;
+    cout<<"Por cada turno el jugador 1 y el jugador tiraran el dado..."<<endl;
+    cout<<"Presiona enter para comenzar el juego: ";
+    cin.get();
+    system("cls");
+
+    do {
+        cout<<rondas<<"._Ronda: ";
+        cin.get();
+        system("cls");
+
+        jugada1=rand()%6+1;
+        dadoAnimacion();
+        
+		cout<<"Tu dado que salio es de "<<jugada1<<" caras~!!!"<<endl<<endl;
 		
-		jugada1=rand()%6+1;
-		cout<<"Tu dado que salio es de "<<jugada1<<" caras..."<<endl<<endl;
-		Sleep(1000);
-		
-		cout<<"Presiona enter para continuar el juego (jugador 2): ";
-		cin.get();
-		system("cls");
-		
-		jugada2=rand()%6+1;
-		cout<<"Tu dado que salio es de "<<jugada2<<" caras..."<<endl;
-		Sleep(1000);
-		rondas++;
-		
-		if(jugada1>jugada2){
+        switch(jugada1){
+        	case 1:{
+        		dado1();
+				break;
+			}
+			case 2:{
+				dado2();
+				break;
+			}
+			case 3:{
+				dado3();
+				break;
+			}
+        	case 4:{
+        		dado4();
+				break;
+			}
+			case 5:{
+				dado5();
+				break;
+			}
+			case 6:{
+				dado6();
+				break;
+			}
+			default:{
+				cout<<"Error"<<endl;
+				break;
+			}
+		}
+        Sleep(2500);
+
+        cout<<"Presiona enter para continuar el juego (jugador 2): ";
+        cin.get();
+        system("cls");
+
+        jugada2=rand()%6+1;
+        dadoAnimacion();
+        
+        cout<<"Tu dado que salio es de "<<jugada2<<" caras~!!!"<<endl<<endl;
+        switch(jugada2){
+        	case 1:{
+        		dado1();
+				break;
+			}
+			case 2:{
+				dado2();
+				break;
+			}
+			case 3:{
+				dado3();
+				break;
+			}
+        	case 4:{
+        		dado4();
+				break;
+			}
+			case 5:{
+				dado5();
+				break;
+			}
+			case 6:{
+				dado6();
+				break;
+			}
+			default:{
+				cout<<"Error"<<endl;
+				break;
+			}
+		}
+        Sleep(2500);
+        rondas++;
+
+        if(jugada1>jugada2) {
             cout << "El jugador 1 ha sumado 1 punto..."<<endl;
             wins1++;
-            Sleep(1000);
+            Sleep(2500);
             system("cls");
-        }else if(jugada1<jugada2){
+        } else if(jugada1<jugada2) {
             cout << "El jugador 2 ha sumado 1 punto..."<<endl;
-            Sleep(1000);
-			system("cls");
+            Sleep(2500);
+            system("cls");
             wins2++;
-        }else if(jugada1==jugada2){
-        	cout<<"Empate~!!!"<<endl;
-        	Sleep(1000);
-			system("cls");
-		}
-		
-	}while(wins1<3 && wins2<3);
-	
-	if(wins1>wins2){
-		cout<<"Felicidades, Jugador 1 gana~!!!"<<endl;
-	} else{
-		cout<<"Felicidades, Jugador 2 gana~!!!"<<endl;
-	}
-	
-	return 0;
+        } else if(jugada1==jugada2) {
+            cout<<"Empate~!!!"<<endl;
+            Sleep(2500);
+            system("cls");
+        }
+
+    } while(wins1<3 && wins2<3);
+
+    if(wins1>wins2) {
+        cout<<"Felicidades, Jugador 1 gana~!!!"<<endl;
+    } else {
+        cout<<"Felicidades, Jugador 2 gana~!!!"<<endl;
+    }
+
+    return 0;
 }
